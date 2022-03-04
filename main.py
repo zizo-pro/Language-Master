f0d24169de1febc22a4bc06401475f57e1acebd4
unknown <ziadezz12@gmail.com>
2022-03-05 00:35:50 +0200

    continued coding the Dict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import mysql.connector
+import mysql.connector
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUiType
 from PyQt5.QtWidgets import *
@@ -9,12 +9,9 @@ from PyQt5.QtCore import QStringListModel
 from os import path
 from sys import argv,exit
 from qt_material import apply_stylesheet
-# db = mysql.connector.connect(
-#   host = "localhost",
-#   user = "ziad",
-#   passwd = "@zizo2412@",
-#   database = "german_database"
-# )
+from speech import deutsch_lang
+
+
 FORM_CLASS,_=loadUiType(path.join(path.dirname(__file__),"GUI/main-gui-file.ui"))
 
 class mainapp(QMainWindow,FORM_CLASS):
@@ -22,6 +19,11 @@ class mainapp(QMainWindow,FORM_CLASS):
         super(mainapp,self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.initialize()
+        self.button_setup()
+        self.Database()
+    
+    def initialize(self):
         self.comboBox.setEditable(True)
         self.comboBox.setFocusPolicy(Qt.StrongFocus)
         self.comboBox.completer().setCompletionMode(QCompleter.PopupCompletion)
@@ -32,13 +34,67 @@ class mainapp(QMainWindow,FORM_CLASS):
         self.completer = QCompleter(self.comboBox.pFilterModel, self)
         self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
         self.comboBox.setCompleter(self.completer)
+        self.comboBox.setCurrentText("")
         self.comboBox.lineEdit().textEdited.connect(self.comboBox.pFilterModel.setFilterFixedString)
         self.completer.activated.connect(self.on_completer_activated)
+        self.first_open()
+    
+    def button_setup(self):
+        self.search_BT.clicked.connect(self.search)
+        self.speaker_BT.clicked.connect(self.speaker)
+
+    def first_open(self):
+        self.article_DB.hide()
+        self.article_LB.hide()
+        self.category_DB.hide()
+        self.category_LB.hide()
+        self.meaning_DB.hide()
+        self.meaning_LB.hide()
+        self.plural_DB.hide()
+        self.plural_LB.hide()
+        self.type_DB.hide()
+        self.type_LB.hide()
+        self.word_DB.hide()
+        self.word_LB.hide()
+        self.setGeometry(200,200,335,75)
+        self.comboBox.setGeometry(15,20,230,25)
+        self.search_BT.setGeometry(255,20,30,25)
+        self.speaker_BT.setGeometry(290,20,30,25)
+
+    def search(self):
+        self.article_DB.show()
+        self.article_LB .show()
+        self.category_DB.show()
+        self.category_LB.show()
+        self.meaning_DB.show()
+        self.meaning_LB.show()
+        self.plural_DB.show()
+        self.plural_LB.show()
+        self.type_DB.show()
+        self.type_LB.show()
+        self.word_DB.show()
+        self.word_LB.show()
+        self.setGeometry(100,100,440,300)
+        self.comboBox.setGeometry(80,20,230,25)
+        self.search_BT.setGeometry(315,20,30,25)
+        self.speaker_BT.setGeometry(350,20,30,25)
+        self.cr.execute(f"SELECT * FROM nouns WHERE noun = '{self.comboBox.currentText()}'")
+        self.data = self.cr.fetchone()
+        self.article_DB.setText(self.data[0])
+        self.word_DB.setText(self.data[1])
+        self.meaning_DB.setText(self.data[2])
+        self.type_DB.setText(self.data[3])
+        self.plural_DB.setText(self.data[4])
+        self.category_DB.setText(self.data[5])
+    
+    def speaker(self):
+        deutsch_lang(self.comboBox.currentText())
+
     def on_completer_activated(self, text):
         if text:
             index = self.comboBox.findText(text)
-            self.setCurrentIndex(index)
-            self.activated[str].emit(self.itemText(index))
+            self.comboBox.setCurrentIndex(index)
+            self.comboBox.activated[str].emit(self.comboBox.itemText(index))
             print(index)
 
     def setModel(self, model):
@@ -51,6 +107,17 @@ class mainapp(QMainWindow,FORM_CLASS):
         self.pFilterModel.setFilterKeyColumn(column)
         super(ExtendedComboBox, self).setModelColumn(column) 
 
+    def Database(self):
+        self.db = mysql.connector.connect(
+          host = "localhost",
+          user = "ziad",
+          passwd = "@zizo2412@",
+          database = "german_database"
+        )
+        self.cr = self.db.cursor()
+        self.cr.execute("SELECT noun FROM nouns")
+        self.combodata = self.cr.fetchall()
+
 
 
 
@@ -58,9 +125,9 @@ if __name__ == "__main__":
     app = QApplication(argv)
     MainWindow = QtWidgets.QMainWindow()
     window = mainapp()
-    string_list = ['hola muchachos',
-                   'adios amigos', 'hello world', 'good bye',
-                   'more data', 'just','maro','zizo','potato','das']
+    string_list = [""]
+    for i in window.combodata:
+        string_list.append(i[0])
     apply_stylesheet(app, theme='dark_teal.xml')
     combo = window.comboBox
     combo.addItems(string_list)
